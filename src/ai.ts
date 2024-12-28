bf59eee596705be33bf4171ce2b3f2c3fa0033a9
Bert Rosell <bert@rosell.net>
2024-12-28 12:37:51 -0500

    refactor AiInterface
    ChatRole enum to a string literal type
    constructor now takes an OpenAI instance and model string
    rename prompt to stream prepping for non-streaming o1 models

--- src/ai.ts
+++ src/ai.ts
@@ -2,35 +2,27 @@ import OpenAI, { ClientOptions } from "openai";
 import { ChatCompletionMessageParam } from "openai/resources";
 import { Observable, Subject } from "rxjs";
 
-export enum ChatRole {
-  SYSTEM = 'SYSTEM',
-  USER = 'USER',
-  ASSISTANT = 'ASSISTANT'
-}
+export type ChatRole = 'SYSTEM' | 'USER' | 'ASSISTANT';
+
 export interface Completion {
   role: ChatRole;
   content: string;
 }
 
-export interface ObservableLlmOptions extends ClientOptions {
-  model: string;
-}
-
 export class AiInterface {
-  private openai!: OpenAI;
+  constructor(
+    private openai: OpenAI,
+    private model: string
+  ) {  }
 
-  constructor(private config: ObservableLlmOptions) {
-    this.openai = new OpenAI(this.config);
-  }
-
-  prompt(completions: Completion[]): Observable<string> {
+  stream(completions: Completion[]): Observable<string> {
     const deltas = new Subject<string>();
 
     (async () => {
       try {
         const stream = await this.openai.chat.completions.create({
           messages: completions.map(c => ({ role: c.role.toLowerCase(), content: c.content } as ChatCompletionMessageParam)), 
-          model: this.config.model,
+          model: this.model,
           stream: true,
         });
 
